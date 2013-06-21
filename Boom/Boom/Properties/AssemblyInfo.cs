160b8057fa7b341ec0d53b1443c253dbc72db181
Florian Kaiser <florian.kaiser@floyd-ug.de>
2013-06-22 00:12:30 +0200

    default character fix

--- Boom/Boom/Properties/AssemblyInfo.cs
+++ Boom/Boom/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Resources;
 //      Build-Nummer
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
